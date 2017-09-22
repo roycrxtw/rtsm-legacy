61c7dbc64a45e1c06af43043a7470bba20421b7a
Roy Lu <royvbtw@gmail.com>
2017-09-22 21:27:25 +0800

    Adjust display style
    + Adjust shortcut tile width.
    + Adjust some css styles.

--- rtsm.js
+++ rtsm.js
@@ -2,20 +2,22 @@
 /**
  * Real-time Traffic Speed Monitor of Motorways in Taiwan (RTSM)
  * A traffic data monitor for motorways in Taiwan.
- * author Roy Lu
+ * @author Roy Lu
  * July 2017
  */
 
 'use strict';
 
 const TIMEOUT = 3000;
-var argCache = '-1';	// store query args
+var midCache = null;		// Cache the mid argument
 var isLoading = false;
 var screenOffset = 0;
 
+
 /**
- * Do the actual job that query data from the official site.
- * This function will send a XMLHttpReqeust to the RTSM api service to query data
+ * Do the actual job that query data from the RTSM API service.
+ * 
+ * This function will send an old school XMLHttpReqeust to the RTSM API service to query data
  * from the official site. And the return data will be sent to a callback
  * function setSpeedData() for further process.
  * 
@@ -26,10 +28,10 @@ function doSubmit(mid) {
 		return;
 	}
 	isLoading = true;
-	argCache = mid;		// save mid in cache for refresh
+	midCache = mid;		// save mid in cache for refresh
 	
-	// display loader icon and disable buttons
-	document.getElementById('btn-search').textContent = 'loading...';
+	// Display loading spinner and loading message
+	document.getElementById('btnSearch').textContent = 'loading...';
 	document.getElementById('loader').style.display = 'block';
 	
 	var url = "https://rtsmapi.royvbtw.uk/data/" + mid;
@@ -37,53 +39,46 @@ function doSubmit(mid) {
 	xhr.open('GET', url, true);
 	xhr.timeout = TIMEOUT;
 	xhr.onload = function () {
-		document.getElementById('btn-search').textContent = 'search';
+		document.getElementById('btnSearch').textContent = 'search';
 		document.getElementById('loader').style.display = 'none';
 		isLoading = false;
 		setSpeedData(mid, xhr);
 	};
 	xhr.ontimeout = function () {
-		document.getElementById('btn-search').textContent = 'search';
+		document.getElementById('btnSearch').textContent = 'search';
 		document.getElementById('loader').style.display = 'none';
 		isLoading = false;
 		queryTimeoutHandler(mid);
 	};
 	xhr.onerror = function(){
-		document.getElementById('btn-search').textContent = 'search';
+		document.getElementById('btnSearch').textContent = 'search';
 		document.getElementById('loader').style.display = 'none';
-		console.error("** An error occurred during the transaction");
 		isLoading = false;
+		console.error("** An error occurred during the transaction");
 	};
 	xhr.send();
-
 }
+
 
 /**
  * Refresh page with previous query to RTSM api.
- * The motorwayId of previous query saved in argCache.
+ * The motorwayId of previous query saved in midCache.
  * This function simply send the cached motorwayId to doSubmit().
- * 
- * @returns {undefined} void
  */
 function refreshPage(){
-	if(argCache !== '-1'){
+	if(midCache){
 		screenOffset = $(document).scrollTop();
-		doSubmit(argCache);
+		doSubmit(midCache);
 	}
 }
 
-// Setup every args that doSumit needs.
+
+// A wrapper function for doSubmit()
 function submit() {
 	let m = document.getElementById("menu-motorways");
 	let mid = m.options[m.selectedIndex].value;
-	screenOffset = $("#speedDataTable").offset().top;
+	screenOffset = $("#trafficTable").offset().top;
 	doSubmit(mid);
-}
-
-
-// #todo-roy: timeout handler
-function menuTimeoutHandler(mid) {
-	// #todo
 }
 
 
@@ -92,34 +87,29 @@ function menuTimeoutHandler(mid) {
  * 
  * This function will format the raw data into html layout.
  * @param {string} argName the section name
- * @param {string} argSpeedA SpeedA, this direction will be set in
- *	initSpeedDataTable().
- * @param {string} argSpeedB SpeedB, this direction will be set in
- *	initSpeedDataTable().
- * @returns {undefined} void
+ * @param {string} argSpeedA SpeedA, this direction will be set in initTrafficTable().
+ * @param {string} argSpeedB SpeedB, this direction will be set in initTrafficTable().
  */
 function printSpeedData(argName, argSpeedA, argSpeedB) {
-	var name = "<div class='d-name'>" + argName + "</div>";
-	var speedA = "<div class='d-speed' data-value='" + argSpeedA + "'>" + argSpeedA + "</div>";
-	var speedB = "<div class='d-speed' data-value='" + argSpeedB + "'>" + argSpeedB + "</div>";
-	var row = "<div class='d-row'>" + name + speedA + speedB + "</div>";
+	const name = "<div class='d-name'>" + argName + "</div>";
+	const speedA = "<div class='d-speed' data-value='" + argSpeedA + "'>" + argSpeedA + "</div>";
+	const speedB = "<div class='d-speed' data-value='" + argSpeedB + "'>" + argSpeedB + "</div>";
+	const row = "<div class='d-row'>" + name + speedA + speedB + "</div>";
 
-	document.getElementById("speedDataTable").innerHTML += row;
+	document.getElementById("trafficTable").innerHTML += row;
 }
 
 
 /**
- * Init the speed data table by motorway id(mid)
+ * Init the traffic table by motorway id and name
  * 
- * This function will init the corresponding labels and information 
- * for the table.
+ * This function will init the corresponding labels and information for the table.
  * @param {string} mid Motorway ID.
  * @param {string} name Motorway name.
  * @param {string} direction The direction of the motorway
- * @returns {undefined} void
  */
-function initSpeedDataTable(mid, name, direction) {
-	var table = document.getElementById("speedDataTable");
+function initTrafficTable(mid, name, direction) {
+	const table = document.getElementById("trafficTable");
 	table.innerHTML = '';
 
 	// set page created time
@@ -127,37 +117,37 @@ function initSpeedDataTable(mid, name, direction) {
 			+ '<span class="motorwayName">' + name + '</span></div>';
 
 	if (mid === undefined) {
-		console.error("setTableDirection(): mid is undefined.");
+		console.error("initTrafficTable(): mid is undefined.");
 		table.innerHTML = "<div>請重新查詢. Please re-submit.</div>";
 		return;
 	}
 
 	var row = "<div class='d-row'>";
-	var nameElement = "<span class='d-name'>路段</span>";
+	const nameElement = "<span class='d-name'>路段</span>";
 	row += nameElement;
 	if (direction === 0) {		// 0: N-S direction, 1: E-W direction.
 		row += "<span class='direction-label'>南向</span><span class='direction-label'>北向</span>";
 	} else if (direction === 1) {
 		row += "<span class='direction-label'>東向</span><span class='direction-label'>西向</span>";
 	} else {
-		console.error("setTableDirection(): An error happened when set the direction.");
+		console.error("initTrafficTable(): An error happened when set the direction.");
 	}
-	row += "</div>";
+	row += '</div>';
 
 	table.innerHTML += row;
 }
 
+
 /**
  * 
- * @param {type} mid The motorway id
- * @param {type} result A json data received from RTSM api
- * @return {undefined}
+ * @param {string} mid The motorway id
+ * @param {object} result A json data received from RTSM api
  */
 function setSpeedData(mid, result) {
-	let json = JSON.parse(result.responseText);
-	initSpeedDataTable(mid, json.name, json.direction);	// reset speed data table
+	const json = JSON.parse(result.responseText);
+	initTrafficTable(mid, json.name, json.direction);	// reset speed data table
 	
-	let trafficData = json.traffic;		// it should be an array
+	const trafficData = json.traffic;		// it should be an array
 
 	for (var i = 0; i < trafficData.length; i++) {
 		let section = trafficData[i];
@@ -166,14 +156,15 @@ function setSpeedData(mid, result) {
 
 	setSpeedColor();
 	
-	// set screen position to table
+	// Scroll screen position to the table
 	$("html body").animate({
 		scrollTop: screenOffset
 	}, 1000);
 }
 
+
 function setSpeedColor() {
-	Array.from(document.getElementsByClassName("d-speed")).forEach(function (item) {
+	Array.from(document.getElementsByClassName("d-speed")).forEach(item => {
 		if (item.dataset.value > 80) {
 			item.classList.add("goodSpeed");
 		} else if (item.dataset.value < 50) {
@@ -184,13 +175,14 @@ function setSpeedColor() {
 	});
 }
 
-// event listenters
+
+// **Event listenters**
 document.getElementById("btnRefresh").addEventListener("click", function () {
 	refreshPage();
 });
 
 $('.tile-shortcut').click(function(){
 	let mid = $(this).data('mid');
-	screenOffset = $("#speedDataTable").offset().top;
+	screenOffset = $("#trafficTable").offset().top;
 	doSubmit(mid);
 });
